6732ec071738003d8f4a4f560043b81ea541fcca
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2021-01-26 16:58:54 +0530

    Add files via upload

---
+++ game/Assets/main.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode()
+pygame.display.set_caption("First Game")
+
+run = False
+
+while run:
+    for event in pygame.eent.get():
+        if event.type == pygame.QUIT:
+
+
+
+
+
+
+            
